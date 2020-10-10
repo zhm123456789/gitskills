40ddbb5fffc51eeb62de829afb30fdbff270417b
zhm123456789 <1241217701@qq.com>
2020-10-10 11:19:42 +0800

    git111

--- src/main/java/gitgit/gitgit/Hello.java
+++ src/main/java/gitgit/gitgit/Hello.java
@@ -1,11 +1,12 @@
 package gitgit.gitgit;
 
 public class Hello {
-	
+
+
 	public static void main(String[] args) {
 		
 		System.out.println("hello world");
-		System.out.println("11111111111");
+		
 		System.out.println("hello git");
-	}
+}
 }
